d9ce53ac75b9c6c83dc70a68ee5fc8cf81c4e4e0
Kenzo Aaron Dela Cruz <kenzo.delacruz03@gmail.com>
2023-10-05 17:37:18 +0800

    Added a method to compare the player's guesses and the color and check if the player won

--- mastermind.rb
+++ mastermind.rb
@@ -15,20 +15,38 @@ def mastermind(colors)
     print random_array
     puts ""
 
-    def checkColor(array, guess)
+    def checkColor(array, guess, result)
+
+        index = 0
 
 
-
-        array.each_index do |index|
-            if guess[index] == array[index]
-                puts "Hello"
+        while index < array.length
+            array.each_index do |color|
+                if guess[index] == array[color]
+                    if index == color
+                        result.append("Black")
+                    elsif index != color
+                        result.append("White")
+                    end
+                end
             end
+            index += 1
         end
+        index = 0
+        return result
+    end
 
+    def checkResult(result)
+        if result[0] == "Black" && result[1] == "Black" && result[2] == "Black" && result[3] == "Black"
+            puts "You Won!"
+            return true
+        else
+            return false
+        end
     end
 
 
-    while tries < 10
+    while guessed != true
         puts "What's the first color?"
         first_color = gets.chomp
         guess_array.push(first_color)
@@ -45,9 +63,15 @@ def mastermind(colors)
         fourth_color = gets.chomp
         guess_array.push(fourth_color)
 
-        checkColor(random_array, guess_array)
-        tries += 10
-        puts tries
+        checkColor(random_array, guess_array,result)
+        guessed = checkResult(result)
+        
+        tries += 1
+        
+        if tries >= 10
+            puts "You have lost"
+            guessed = true
+        end
     end  
 
     
