e5313218bbdf2a34202471cbcf8d9de300a9e4f5
Kenzo Aaron Dela Cruz <kenzo.delacruz03@gmail.com>
2023-10-05 10:53:42 +0800

    Added an array for the player's guesses

--- mastermind.rb
+++ mastermind.rb
@@ -5,18 +5,51 @@ colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def mastermind(colors)
 
     random_array = colors.sample(4)
+
+    result = []
+    guess_array = []
+    
+    guessed = false
     tries = 0
-
-    while tries < 10
-        puts "What's your guess?"
-
-        guess = gets.chomp
-        tries += 1
-    end  
 
     print random_array
     puts ""
 
+    def checkColor(array, guess)
+
+
+
+        array.each_index do |color|
+            if guess == array[color]
+            end
+        end
+
+    end
+
+
+    while tries < 10
+        puts "What's the first color?"
+        first_color = gets.chomp
+        guess_array.push(first_color)
+
+        puts "What's the second color?"
+        second_color = gets.chomp
+        guess_array.push(second_color)
+
+        puts "What's the third color?"
+        third_color = gets.chomp
+        guess_array.push(third_color)
+
+        puts "What's the fourth color?"
+        fourth_color = gets.chomp
+        guess_array.push(fourth_color)
+
+        print guess_array
+        tries += 10
+        puts tries
+    end  
+
+    
 end
 
 mastermind(colors)
