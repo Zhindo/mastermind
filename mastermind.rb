665b80dd2f6c20c37e5321875f44b29d531ebfee
Kenzo Aaron Dela Cruz <kenzo.delacruz03@gmail.com>
2023-10-06 14:38:54 +0800

    Added a choice between playing or making the computer guess

--- mastermind.rb
+++ mastermind.rb
@@ -16,7 +16,7 @@ end
 # List of colors to choose from. 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-def mastermind(colors)
+def mastermindPlayer(colors)
 
     random_array = colors.sample(4)
 
@@ -47,6 +47,8 @@ def mastermind(colors)
             index += 1
         end
         index = 0
+        results = result.sort { |b, a| b <=> a }
+        puts results
         return result
     end
 
@@ -82,8 +84,8 @@ def mastermind(colors)
         
         tries += 1
         
-        if tries >= 10
-            puts "You have lost"
+        if tries >= 12
+            puts "You have lost. Try Again"
             guessed = true
         end
     end  
@@ -91,4 +93,36 @@ def mastermind(colors)
     
 end
 
-mastermind(colors)
+puts "What's your name?"
+
+player = Player.new("#{gets.chomp}")
+
+puts player.name
+
+puts "Enter 1 if you want to play as the player"
+puts "Enter 2 if you want the Computer to guess you."
+print "\nYour Choice: "
+
+correct_choice = false
+choice = gets.chomp.to_i
+
+while correct_choice != true
+    
+    if choice == 1 || choice == 2
+        correct_choice = true
+    else
+        puts choice
+        puts "Wrong choice. Try again"
+        puts "\nEnter 1 if you want to play as the player"
+        puts "Enter 2 if you want the Computer to guess you."
+        print "\nYour Choice: "
+        choice = gets.chomp.to_i
+    end
+end
+
+if choice == 1
+    mastermindPlayer(colors)
+else
+    mastermindComputer(colors)
+end
+
